906190ba5cf987c86786146453a2860a68ce60a8
Haydz6 <cookiegamespa@gmail.com>
2022-09-03 18:18:01 +1000

    renamed to fetchfiles and added settings

---
+++ fetchfiles.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"path"
+)
+
+type SettingsStruct struct {
+	DeleteGDPRMessagesAfterFulfilled bool
+	CreateLogFile                    bool
+}
+
+var DataKeys map[string]map[string]map[string][]string
+var Settings SettingsStruct
+
+func FetchDataKeys() {
+	Bytes, err := os.ReadFile(path.Join(".", "DataKeys.json"))
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	json.Unmarshal(Bytes, &DataKeys)
+}
+
+func FetchSettings() {
+	Bytes, err := os.ReadFile(path.Join(".", "Settings.json"))
+
+	if err != nil {
+		println(err.Error())
+	}
+
+	json.Unmarshal(Bytes, &Settings)
+}
